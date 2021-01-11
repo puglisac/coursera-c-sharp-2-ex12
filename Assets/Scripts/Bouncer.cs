e6d71d36f03875fb7be5fb7278d60d43d8679479
Alan Puglisi <alan.c.puglisi@gmail.com>
2021-01-11 12:55:54 -0600

    exercise complete

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    int health = 100;
+
+    int Health { get { return health; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(new Vector2(50, 100));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        health -= 10;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
